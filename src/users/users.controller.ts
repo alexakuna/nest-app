7289b82e00a7b38721c5bd12d2984b42835da014
Alexandr <alexxjc@gmail.com>
2022-05-25 16:29:04 +0300

    Init module roles.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,20 +4,29 @@ import {CreateUserDto} from "./dto/create-user.dto";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./user.model";
 
+//Swagger API documentation
 @ApiTags('Users')
+
+//Route endpoint
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
+    //Swagger API documentation
     @ApiOperation({summary: 'Create user'})
     @ApiResponse({status: 200, type: User})
+
+    //API method
     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
 
+    //Swagger API documentation
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [User]})
+
+    //API method
     @Get()
     getAll() {
         return this.usersService.getAllUsers();
